769ca39edbb07b0a8915146130c535e8b83a0008
birdee <birdee@birdee.com>
2024-11-15 22:55:29 -0800

    --help
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   wez/init.lua
    	new file:   wez/sessionizer.lua

--- wez/init.lua
+++ wez/init.lua
@@ -1,39 +1,119 @@
--- See https://wezfurlong.org/wezterm/
-local fromnix = require 'nixStuff'
-local wezterm = require 'wezterm'
-local config = wezterm.config_builder()
+local wezterm = require("wezterm")
+local sessionizer = require("sessionizer")
 
-config.font_size = 11.0
-config.color_scheme = 'Oceanic Next (Gogh)'
-config.font = wezterm.font(fromnix.nerdString .. ' Nerd Font')
-config.color_scheme_dirs = { wezterm.config_dir .. "/colors" }
-config.font_dirs = fromnix.fontDirs
-config.keys = {}
-config.set_environment_variables = fromnix.envVars
-config.allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace"
-config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
+return {
+	font = wezterm.font_with_fallback({
+		"IosevkaTerm Nerd Font",
+		"Atkinson Hyperlegible",
+	}),
+	font_size = 14.0,
+	color_scheme = "kanagawa_custom",
+	use_ime = true,
+	enable_kitty_keyboard = true,
+	window_background_opacity = 0.7,
+	window_decorations = "NONE",
+	window_close_confirmation = "NeverPrompt",
+	enable_tab_bar = false,
+	webgpu_power_preference = "HighPerformance",
+	check_for_updates = false,
+	enable_wayland = true,
+	max_fps = 165,
+	anti_alias_custom_block_glyphs = true,
+	default_cursor_style = "SteadyBlock",
+	warn_about_missing_glyphs = true,
+	tiling_desktop_environments = {
+		"Wayland Hyprland",
+	},
+	window_padding = {
+		left = "0px",
+		right = "0px",
+		top = "0px",
+		bottom = "0px",
+	},
+	hyperlink_rules = {
+		-- Matches: a URL in parens: (URL)
+		{
+			regex = "\\((\\w+://\\S+)\\)",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Matches: a URL in brackets: [URL]
+		{
+			regex = "\\[(\\w+://\\S+)\\]",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Matches: a URL in curly braces: {URL}
+		{
+			regex = "\\{(\\w+://\\S+)\\}",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Matches: a URL in angle brackets: <URL>
+		{
+			regex = "<(\\w+://\\S+)>",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Then handle URLs not wrapped in brackets
+		{
+			regex = "\\b\\w+://\\S+[)/a-zA-Z0-9-]+",
+			format = "$0",
+		},
+		-- implicit mailto link
+		{
+			regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+			format = "mailto:$0",
+		},
+	},
+	keys = {
+		{
+			key = "\\",
+			mods = "ALT",
+			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = "Q",
+			mods = "ALT|SHIFT",
+			action = wezterm.action.CloseCurrentPane({ confirm = false }),
+		},
+		{
+			key = "RightArrow",
+			mods = "ALT|SHIFT",
+			action = wezterm.action.ActivatePaneDirection("Right"),
+		},
+		{
+			key = "LeftArrow",
+			mods = "ALT|SHIFT",
+			action = wezterm.action.ActivatePaneDirection("Left"),
+		},
+		{
+			key = "q",
+			mods = "ALT",
+			action = wezterm.action.CloseCurrentTab({ confirm = false }),
+		},
+		{
+			key = "a",
+			mods = "ALT",
+			action = wezterm.action.SpawnTab("DefaultDomain"),
+		},
+		{
+			key = "c",
+			mods = "ALT",
+			action = wezterm.action.ActivateCommandPalette,
+		},
+		{
+			key = "/",
+			mods = "ALT",
+			action = wezterm.action.Search("CurrentSelectionOrEmptyString"),
+		},
+		{
+			key = "s",
+			mods = "ALT",
+			action = wezterm.action.ShowTabNavigator,
+		},
+		-- Sessionizer Bindings
+		{ key = "f", mods = "ALT", action = wezterm.action_callback(sessionizer.toggle) },
+		-- { key = "F", mods = "ALT", action = wezterm.action_callback(sessionizer.resetCacheAndToggle) },
+	},
 }
-config.window_decorations = "NONE"
--- config.window_decorations = "TITLE"
--- config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
--- config.window_decorations = "TITLE | RESIZE"
--- config.window_decorations = "RESIZE"
-config.window_close_confirmation = 'NeverPrompt'
-config.default_prog = fromnix.shellString
-config.adjust_window_size_when_changing_font_size = false
-config.enable_scroll_bar = true
-config.enable_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = true
-config.use_fancy_tab_bar = false
-config.show_tabs_in_tab_bar = false
-config.tab_bar_at_bottom = false
-config.front_end = "Software"
--- config.front_end = "OpenGL"
--- config.front_end = "WebGpu"
-config.command_palette_rows = 0
-return config
-
