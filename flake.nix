1e38b9fdad47005536d8eae965d851d83c94d0f3
BirdeeHub <birdee@localhost>
2025-10-26 03:54:00 -0700

    Squashed a bug that only happened when you left your shoes on the door
    (auto-msg qwen2.5-coder:7b)
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixToLua.url = "github:BirdeeHub/nixToLua";
-    # wezterm.url = "github:wez/wezterm?dir=nix";
   };
   outputs = { self, nixpkgs, nixToLua, ... }@inputs: let
     forAllSys = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
@@ -14,14 +13,7 @@
       default = pkgs.callPackage ./wez {
         inherit tmux nixToLua;
         wrapZSH = true;
-        wezterm = pkgs.wezterm; #.packages.${system}.default.overrideAttrs {
-          # preFixup = '''';
-          # postFixup = ''
-          #   patchelf \
-          #     --add-rpath "${pkgs.libGL}/lib/libEGL.so.1:${pkgs.vulkan-loader}/lib/libvulkan.so.1" \
-          #     $out/bin/wezterm-gui
-          # '';
-        # };
+        wezterm = pkgs.wezterm;
       };
       wezterm = self.packages.${system}.default.override {
         wrapZSH = false;
