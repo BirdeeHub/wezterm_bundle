c0aa75212458c4c2f3f9d37af3a8a142b5db5649
BirdeeHub <birdee@localhost>
2025-11-05 02:54:45 -0800

    Added a feature to make coffee taste like rainbows
    (auto-msg qwen2.5-coder:7b)
    On branch vimnav
    Your branch is up to date with 'origin/vimnav'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   flake.nix
    	deleted:    tmux-navigate/tmux-navigate.tmux
    	modified:   tmux.nix

--- flake.nix
+++ flake.nix
@@ -2,13 +2,19 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixToLua.url = "github:BirdeeHub/nixToLua";
+    tmux-navigate-src = {
+      url = "github:sunaku/tmux-navigate";
+      flake = false;
+    };
   };
   outputs = { self, nixpkgs, nixToLua, ... }@inputs: let
     forAllSys = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
   in {
     packages = forAllSys (system: let
       pkgs = import nixpkgs { inherit system; };
-      tmux = pkgs.callPackage ./tmux.nix {};
+      tmux = pkgs.callPackage ./tmux.nix {
+        inherit (inputs) tmux-navigate-src;
+      };
     in{
       default = pkgs.callPackage ./wez {
         inherit tmux nixToLua;
